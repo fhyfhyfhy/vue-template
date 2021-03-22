557028c1620f6fcd16a15aac487b9d2d1284e190
Feng Huiyuan <2385422732@qq.com>
2021-03-22 10:19:16 +0800

    feat

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,11 +1,26 @@
 <template>
-  <div>hhh</div>
+  <div :class="classObj" class="app-wrapper"></div>
 </template>
 
 <script>
 export default {
   name: 'Layout',
+  computed: {
+    classObj() {
+      return {};
+    },
+  },
 };
 </script>
 
-<style></style>
+<style lang="scss" scoped>
+@import '@/styles/mixin.scss';
+@import '@/styles/variables.scss';
+
+.app-wrapper {
+  @include clearfix;
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
+</style>
